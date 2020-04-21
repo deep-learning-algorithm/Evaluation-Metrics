a7a8c9e470ba3cb638d595239a2da5ce494274e5
zhujian <505169307@qq.com>
2020-04-21 11:07:17 +0800

    refactor(map): 提取tp/fp计算和precision/recall计算

--- mAP/voc_map.py
+++ mAP/voc_map.py
@@ -39,6 +39,93 @@ def pretreat(ground_truth_dir, detection_result_dir, tmp_json_dir):
     os.mkdir(tmp_json_dir)
 
 
+def compute_tp_fp(dt_per_classes_dict, MIN_OVERLAP=0.5):
+    dt_list = dt_per_classes_dict[cate]
+    # {cate_1: [], cate2: [], ...}
+    nd = len(dt_list)
+    tp = [0] * nd  # creates an array of zeros of size nd
+    fp = [0] * nd
+    # 遍历所有候选预测框，判断TP/FP/FN
+    for idx, dt_data in enumerate(dt_list):
+        # {"confidence": "0.999", "file_id": "cucumber_61", "bbox": [16, 42, 225, 163]}
+        # 读取保存的信息
+        file_id = dt_data['file_id']
+        dt_bbox = dt_data['bbox']
+        confidence = dt_data['confidence']
+
+        # 读取对应文件的真值标注框
+        gt_path = os.path.join(tmp_json_dir, file_id + ".json")
+        gt_data = json.load(open(gt_path))
+
+        # 逐个计算预测边界框和对应类别的真值标注框的IoU，得到其对应最大IoU的真值标注框
+        ovmax = -1
+        gt_match = -1
+        # load detected object bounding-box
+        for obj in gt_data:
+            # {"cate": "cucumber", "bbox": [23, 42, 206, 199], "used": true}
+            # 读取保存的信息
+            obj_cate = obj['cate']
+            obj_bbox = obj['bbox']
+            obj_used = obj['used']
+
+            # look for a class_name match
+            if obj_cate == cate:
+                bi = [max(dt_bbox[0], obj_bbox[0]), max(dt_bbox[1], obj_bbox[1]),
+                      min(dt_bbox[2], obj_bbox[2]), min(dt_bbox[3], obj_bbox[3])]
+                iw = bi[2] - bi[0] + 1
+                ih = bi[3] - bi[1] + 1
+                if iw > 0 and ih > 0:
+                    # compute overlap (IoU) = area of intersection / area of union
+                    ua = (dt_bbox[2] - dt_bbox[0] + 1) * (dt_bbox[3] - dt_bbox[1] + 1) + \
+                         (obj_bbox[2] - obj_bbox[0] + 1) * (obj_bbox[3] - obj_bbox[1] + 1) \
+                         - iw * ih
+                    ov = iw * ih / ua
+                    if ov > ovmax:
+                        ovmax = ov
+                        gt_match = obj
+        # 如果大于最小IoU阈值，还需要进一步判断是否为TP
+        if ovmax >= MIN_OVERLAP:
+            if not bool(gt_match["used"]):
+                # true positive
+                tp[idx] = 1
+                gt_match["used"] = True
+                # update the ".json" file
+                with open(gt_path, 'w') as f:
+                    json.dump(gt_data, f)
+            else:
+                # false positive (multiple detection)
+                fp[idx] = 1
+        else:
+            # false positive
+            fp[idx] = 1
+
+    return tp, fp
+
+
+def compute_precision_recall(tp, fp, gt_per_classes_num):
+    """
+    计算不同阈值下的precision/recall
+    """
+    # compute precision/recall
+    cumsum = 0
+    for idx, val in enumerate(fp):
+        fp[idx] += cumsum
+        cumsum += val
+    cumsum = 0
+    for idx, val in enumerate(tp):
+        tp[idx] += cumsum
+        cumsum += val
+
+    rec = tp[:]
+    for idx, val in enumerate(tp):
+        rec[idx] = float(val) / gt_per_classes_num
+    # print(rec)
+    prec = tp[:]
+    for idx, val in enumerate(tp):
+        prec[idx] = float(tp[idx]) / (fp[idx] + tp[idx])
+    return prec, rec
+
+
 def voc_ap(rec, prec):
     """
     --- Official matlab code VOC2012---
@@ -137,89 +224,13 @@ if __name__ == '__main__':
     dt_per_classes_dict = file.parse_detection_results(detection_result_dir, tmp_json_dir)
 
     MIN_OVERLAP = 0.5
-    count_true_positives = {}
     # 计算每个类别的tp/fp
     sum_AP = 0.0
 
     for cate in gt_classes:
-        dt_list = dt_per_classes_dict[cate]
-        # {cate_1: [], cate2: [], ...}
-        nd = len(dt_list)
-        tp = [0] * nd  # creates an array of zeros of size nd
-        fp = [0] * nd
-        # 遍历所有候选预测框，判断TP/FP/FN
-        for idx, dt_data in enumerate(dt_list):
-            count_true_positives[cate] = 0
-            # {"confidence": "0.999", "file_id": "cucumber_61", "bbox": [16, 42, 225, 163]}
-            # 读取保存的信息
-            file_id = dt_data['file_id']
-            dt_bbox = dt_data['bbox']
-            confidence = dt_data['confidence']
+        tp, fp = compute_tp_fp(dt_per_classes_dict, MIN_OVERLAP=MIN_OVERLAP)
 
-            # 读取对应文件的真值标注框
-            gt_path = os.path.join(tmp_json_dir, file_id + ".json")
-            gt_data = json.load(open(gt_path))
-
-            # 逐个计算预测边界框和对应类别的真值标注框的IoU，得到其对应最大IoU的真值标注框
-            ovmax = -1
-            gt_match = -1
-            # load detected object bounding-box
-            for obj in gt_data:
-                # {"cate": "cucumber", "bbox": [23, 42, 206, 199], "used": true}
-                # 读取保存的信息
-                obj_cate = obj['cate']
-                obj_bbox = obj['bbox']
-                obj_used = obj['used']
-
-                # look for a class_name match
-                if obj_cate == cate:
-                    bi = [max(dt_bbox[0], obj_bbox[0]), max(dt_bbox[1], obj_bbox[1]),
-                          min(dt_bbox[2], obj_bbox[2]), min(dt_bbox[3], obj_bbox[3])]
-                    iw = bi[2] - bi[0] + 1
-                    ih = bi[3] - bi[1] + 1
-                    if iw > 0 and ih > 0:
-                        # compute overlap (IoU) = area of intersection / area of union
-                        ua = (dt_bbox[2] - dt_bbox[0] + 1) * (dt_bbox[3] - dt_bbox[1] + 1) + \
-                             (obj_bbox[2] - obj_bbox[0] + 1) * (obj_bbox[3] - obj_bbox[1] + 1) \
-                             - iw * ih
-                        ov = iw * ih / ua
-                        if ov > ovmax:
-                            ovmax = ov
-                            gt_match = obj
-            # 如果大于最小IoU阈值，还需要进一步判断是否为TP
-            if ovmax >= MIN_OVERLAP:
-                if not bool(gt_match["used"]):
-                    # true positive
-                    tp[idx] = 1
-                    gt_match["used"] = True
-                    count_true_positives[cate] += 1
-                    # update the ".json" file
-                    with open(gt_path, 'w') as f:
-                        json.dump(gt_data, f)
-                else:
-                    # false positive (multiple detection)
-                    fp[idx] = 1
-            else:
-                # false positive
-                fp[idx] = 1
-
-        # compute precision/recall
-        cumsum = 0
-        for idx, val in enumerate(fp):
-            fp[idx] += cumsum
-            cumsum += val
-        cumsum = 0
-        for idx, val in enumerate(tp):
-            tp[idx] += cumsum
-            cumsum += val
-
-        rec = tp[:]
-        for idx, val in enumerate(tp):
-            rec[idx] = float(val) / gt_per_classes_dict[cate]
-        # print(rec)
-        prec = tp[:]
-        for idx, val in enumerate(tp):
-            prec[idx] = float(tp[idx]) / (fp[idx] + tp[idx])
+        prec, rec = compute_precision_recall(tp, fp, gt_per_classes_dict[cate])
 
         # ap, mrec, mprec = voc_ap(rec[:], prec[:])
         ap = voc_ap2(rec[:], prec[:])
