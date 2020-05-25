5f39df90f6758629fa89e8d41a58d77fd1b6fb90
zhujian <505169307@qq.com>
2020-05-25 16:31:00 +0800

    refactor(utils): 移除不使用的代码

--- py/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-@date: 2020/4/28 上午9:27
-@file: __init__.py.py
-@author: zj
-@description: 
-"""
--- py/utils/file.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""
-@date: 2020/4/20 上午10:21
-@file: file.py
-@author: zj
-@description: 
-"""
